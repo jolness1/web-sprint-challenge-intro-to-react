be6f4d78674454b28428f0e422f6142eba5e7350
Jacob Olness <jacobolness@Jacobs-MBP.lan>
2020-11-06 11:44:18 -0700

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import Character from "./components/Character";
 import './App.css';
+import styled from "styled-components";
 
-const App = () => {
+  
+const App = () =>{
+  const id=0;
+
+  return (
+    <Container>
+      <h1 className="Header">
+        <span role="img" aria-label="Star Wars">🚀</span>
+        Star Wars Characters
+        <span role="img" aria-label="Star Wars">🚀</span>
+      </h1>
+      <Character key={id}/>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  text-align:center;
+  .Header{
+    font-size:4rem;
+  }
+`
+export default App;
+
+
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,11 +36,4 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
 
-export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,38 @@
 // Write your Character component here
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import Card from "./Card";
+import styled from "styled-components";
+
+
+function Character({key1}) {
+
+    let [characters, getCharacters] = useState([]);
+
+    useEffect(() =>{
+        axios
+            .get("https://swapi.dev/api/people")
+            .then(res=>{
+                console.log(res.data.results);
+                getCharacters(res.data.results);
+            })
+            .catch(err=>console.log(err));
+    },[]);
+
+    return(
+        <Container>
+        <div className="charCard">
+            {characters.length > 0 ? characters.map((char, i) => <Card key={char.id} char={char} />) : <p> Loading ...</p>}
+        </div>
+        </Container>
+    );
+};
+const Container = styled.div`
+    .charCard{
+     display:flex;
+     flex-wrap:wrap;
+     justify-content:space-evenly;
+    }
+`   
+
+export default Character;
